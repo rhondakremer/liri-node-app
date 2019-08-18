a73ca557660e6825e1750bdf8d9321d3611ac643
Rhonda Kremer <rhondakremer@gmail.com>
2019-08-18 13:35:51 -0400

    hooked up Bands in Town API, functioning, still needs moment for display

--- liri.js
+++ liri.js
@@ -5,22 +5,39 @@
 // Here we incorporate the "axios" npm package
 var axios = require("axios");
 
+//OMDB
 if (process.argv[2] == "movie-this") {
-    
-
-var movie = [];
-
-for (i = 3; i < process.argv.length; i++){
+    var movie = [];
+    for (i = 3; i < process.argv.length; i++){
     movie.push(process.argv[i]);
     }
     //console.log(movie)
 
 axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
   function(response) {
-    // Then we print out the imdbRating
-
-    
     console.log("Title: " + response.data.Title + "\nReleased: " + response.data.Released + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " +response.data.Actors);
   }
 );
-};
+} 
+//Bands in Town
+else if (process.argv[2] == "concert-this") {
+    var artist = "";
+    for (i = 3; i < process.argv.length; i++){
+    artist += (process.argv[i]);
+    
+} 
+//console.log(artist);
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    for (let i = 0; i < response.data.length; i++) {
+        console.log("Venue: " + response.data[i].venue.name);
+        console.log("City: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+        console.log("Date: " + response.data[i].datetime + "\n--------");
+    }  
+    
+    
+    
+  }
+);
+}
